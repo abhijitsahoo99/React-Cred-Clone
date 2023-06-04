c56603a242a51e68aa380e46d0acf557b43479db
Abhijit Sahoo <abhijitsahooas.01@gmail.com>
2023-06-04 14:20:53 +0530

    Update index.js
    Code for the hero page of cred website.

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Button from '../Common/Button';
+import "./heroSection.css"
+import Header from '../Common/Header';
 
 const HeroSection = () => {
   return (
-    <div>
-      
+    <div className='hero-section-wrapper'>
+    <Header />
+    <div className='flex absolute-center hero-claim-label'>
+        <div>pay credit card bill. earn guaranteed ₹200 back.</div>
+        <div className='claim-anchor'> claim now ⤢</div>
+    </div>
+        <div className='flex absolute-center flex-col hero-section max-width'>
+            <div className='hero-heading'>
+                rewards for paying credit card bills.
+            </div>
+            <div className='hero-subheading'>
+                join 9M+ members who win rewards and cashbacks everyday
+            </div>
+            <Button buttonText = "Download CRED" />
+        </div>
     </div>
   )
 }
 
 export default HeroSection;
+
